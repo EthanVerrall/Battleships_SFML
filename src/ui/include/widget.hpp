45c30fafc5c90592c943000468bc59b668144315
Ethan Verrall <ejverrall24@gmail.com>
2026-09-19 18:01:48 +0100

    Starting draft on label class, added border struct to base widget class.

--- src/ui/include/widget.hpp
+++ src/ui/include/widget.hpp
@@ -5,7 +5,7 @@
 // ----------------------------------------------------------------------------
 
 #include "SFML/Graphics.hpp"
-
+#include "utils/include/colors.hpp"
 #include <string_view>
 
 // ============================================================================
@@ -36,40 +36,79 @@ struct Widget_data {
     Widget_type type;
 };
 
+struct Widget_border {
+
+    sf::Color border_color;
+    float border_size;
+};
+
 // ============================================================================
 // Class Widget
 // ----------------------------------------------------------------------------
 
 class Widget  {
 
+    //--------------------------
+    // Constructor / Destructor
+    //--------------------------
+protected:
+
+    Widget(
+        sf::RenderWindow & window,
+        Widget_data const& data
+        )
+        : _window(window)
+        , _data(data)
+        , _border(Widget_border{.border_color = utils::colors::DEFAULT_BORDER, 
+                                .border_size = 0.0f }
+        )
+    {}
+
 public:
+
     virtual ~Widget() = default;
+
+    //--------------------------
+    // Class specific functions
+    //--------------------------
+public:
 
     virtual void draw() = 0;
 
     virtual void update(float const dt) = 0;
 
+    //--------------------------
+    // Getters
+    //--------------------------
+public:
+
     Widget_data get_data() const { return _data; }
+
+    Widget_border get_border() const { return _border; }
 
     virtual sf::Vector2f get_pos() const = 0;
 
     virtual sf::Vector2f get_scale() const = 0;
 
+    //--------------------------
+    // Setters
+    //--------------------------
+public:
+    
+    void set_border(const Widget_border widget_border) { _border = widget_border; }
+
     virtual void set_pos(sf::Vector2f const pos) = 0;
 
     virtual sf::Vector2f set_scale(sf::Vector2f const scale) = 0;
-
+    
+    //--------------------------
+    // Attributes
+    //--------------------------
 protected:
-    Widget(
-        sf::RenderWindow const& window,
-        Widget_data const& data
-        )
-        : _window(window)
-        , _data(data)
-    {}
 
-    sf::RenderWindow const& _window;
-    Widget_data _data{};
+    sf::RenderWindow & _window;
+    Widget_data _data;
+    Widget_border _border;
 };
 
 }
--- src/utils/include/colors.hpp
+++ src/utils/include/colors.hpp
@@ -15,6 +15,7 @@ namespace battleships::utils {
 namespace colors {
 
 inline constexpr sf::Color DEFAULT_BACKGROUND(sf::Color::Black);
+inline constexpr sf::Color DEFAULT_BORDER(sf::Color::Magenta);
 inline constexpr sf::Color DEFAULT_TEXT(sf::Color::White);
 
 }
