3ed25e8b6074d3e1724baed49d24ca60547941fe
Ethan Verrall <ejverrall24@gmail.com>
2026-09-19 02:17:16 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            
+            // "Close window if pressing escape
+            if (const auto key_event = event->getIf<sf::Event::KeyPressed>()) {
+
+                if (key_event->code == sf::Keyboard::Key::Escape) {
+                    window.close();
+                }
+            }
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
+        window.display();
+    }
+}
