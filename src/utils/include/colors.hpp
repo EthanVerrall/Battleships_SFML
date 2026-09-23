ba4f89212e1de8765648321e6f70bee2120a46b5
HannesW101 <hannesw101@gmail.com>
2026-09-23 20:37:47 +0200

    Added more colors

--- src/utils/include/colors.hpp
+++ src/utils/include/colors.hpp
@@ -17,6 +17,8 @@ namespace colors {
 inline constexpr sf::Color DEFAULT_BACKGROUND(sf::Color::Black);
 inline constexpr sf::Color DEFAULT_BORDER(sf::Color::Magenta);
 inline constexpr sf::Color DEFAULT_TEXT(sf::Color::White);
+inline constexpr sf::Color DEFAULT_CONTAINER_COLOR(sf::Color::Green);
+inline constexpr sf::Color DEFAULT_FILL_COLOR(sf::Color::Cyan);
 
 }
 
